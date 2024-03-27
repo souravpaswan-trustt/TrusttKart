f27d7beaae34997d809cf128cd14b7f2c0b6ee8c
sourav paswan <sourav.paswan@trustt.com>
2024-03-27 11:54:01 +0530

    custom splash screen

--- app/src/main/java/com/example/trusttkart/MainActivity.kt
+++ app/src/main/java/com/example/trusttkart/MainActivity.kt
@@ -18,8 +18,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(1000)
-        installSplashScreen()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
---
+++ app/src/main/java/com/example/trusttkart/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.trusttkart
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.trusttkart.databinding.ActivitySplashScreenBinding
+
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
